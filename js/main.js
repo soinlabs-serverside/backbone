d3359f7d0d9816f361b5196a2e9b825c62fa1b07
fcerdas <fcerdas@soin.co.cr>
2014-10-15 14:49:32 -0600

    backbone normal

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-﻿Sfotipy = {};
-
-Sfotipy.Song = Backbone.Model.extend({});
-
-Sfotipy.Songs = Backbone.Collection.extend({});
-
-Sfotipy.SongView = Backbone.View.extend({
-	tagName: "li",
-	className: "item border-bottom",
-	template: Handlebars.compile($("#song-template").html()),
-	initialize: function () {
-		this.listenTo(this.model,"change",this.render,this);
-	},
-	events: {
-		'click .action.icon-add' : 'add'
-	},
-	render: function () {
-		var html = this.template(this.model.toJSON());
-		this.$el.html(html);
-	},
-	add: function (e) {
-		e.preventDefault();
-		alert(this.model.get("author"));
-	}
+$(function() {
+  Sfotipy.app = new Sfotipy.Router();
 });
-
-window.Sfotipy = Sfotipy;
